8e6d62481003bf6da36fbd9913d0b6b5e7a8e09e
Admin <sohilasobhysadek@gmail.com>
2025-04-23 14:23:06 +0200

    edits 23/4

--- Index.js
+++ Index.js
@@ -28,69 +28,7 @@ const mainSwiper = new Swiper(".main-swiper", {
 const btn = document.getElementById('toggle-btn');
 const text = document.getElementById('text');
 
-btn.addEventListener('click', () => {
-    text.classList.toggle('expanded');
-    btn.textContent = text.classList.contains('expanded') ? 'See less' : 'See more';
-});
 
-// $(document).ready(function () {
-//     var isRtl = $('html').attr('dir') === 'rtl';
-
-//     $('.slider').slick({
-//         dots: true,
-//         rtl: isRtl,
-//         slidesToShow: 3,
-//         slidesToScroll: 1,
-//         rows: 2,
-//         infinite: true,
-//         autoplay: true,
-//         autoplaySpeed: 3000,
-//         prevArrow: "<button class='prev'><img src='assets/Images/leftArrow.svg' alt='prevBtn'></button>",
-//         nextArrow: "<button class='next'><img src='assets/Images/rightArrow.svg' alt='nextBtn'></button>",
-//         responsive: [
-//             {
-//                 breakpoint: 992,
-//                 settings: {
-//                     slidesToShow: 1,
-//                     slidesToScroll: 1,
-//                     rows: 1
-//                 }
-//             },
-//             {
-//                 breakpoint: 768,
-//                 settings: {
-//                     slidesToShow: 1,
-//                     slidesToScroll: 1,
-//                     rows: 1
-//                 }
-//             },
-//         ]
-//     });
-// });
-// $(document).ready(function () {
-//     var isRtl = $('html').attr('dir') === 'rtl';
-//     $('.slider2').slick({
-//         rtl: isRtl,
-//         slidesToShow: 4,
-//         infinite: true,
-//         prevArrow: "<button class='prev'><img src='assets/Images/leftArrow.svg' alt='prevBtn'></button>",
-//         nextArrow: "<button class='next'><img src='assets/Images/rightArrow.svg' alt='nextBtn'></button>",
-//         responsive: [
-//             {
-//                 breakpoint: 992,
-//                 settings: {
-//                     slidesToShow: 1,
-//                 }
-//             },
-//             {
-//                 breakpoint: 768,
-//                 settings: {
-//                     slidesToShow: 1,
-//                 }
-//             },
-//         ]
-//     });
-// });
 
 
 
